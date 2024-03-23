8ddac6ddff9d01fc95adaf8784212d2ddf7a46db
David Sharp <hello@davidsharp.codes>
2024-03-23 21:49:33 +0000

    Print high score if there is one [2:06:50]

--- splash.gd
+++ splash.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
+func _ready():
+	if Globals.high_score:
+		$Label2.text = $Label2.text + '\n\nHighscore: ' + str(Globals.high_score)
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
